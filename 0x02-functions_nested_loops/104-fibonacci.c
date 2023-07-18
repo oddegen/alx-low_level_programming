aae1d3357e43eaf3c6bf8dc06667ade5afae3bda
Nahom <nahomtensae@gmail.com>
2023-07-18 04:09:27 -0700

    104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,28 @@
-#include "main.h"
+#include"main.h"
+
+/**
+ * numLength - returns the length of string
+ *
+ * @num: operand number
+ *
+ * Return: number of digits
+*/
+
+int numLength(int num)
+{
+	int length = 0;
+
+	if (!num)
+		return (1);
+
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	}
+
+	return (length);
+}
 
 /**
  * main - Entry point
@@ -6,31 +30,43 @@
  * Description: prints the first 98 Fibonacci numbers
  *	starting with 1 and 2 followed by a new line
  *
+ *	Solution was copied from Nobert Patrick
+ *	Wise, github handle: Trikcode
  *
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	int count = 0, fib1 = 1, fib2 = 2;
-	double sum = 0;
+	int count, initial0s;
+	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
 
-	printf("1, 2, ");
-	while (count <= 96)
+	for (count = 1; count <= 98; ++count)
 	{
-		sum = fib1 + fib2;
-		++count;
+		if (f1o > 0)
+			printf("%lu", f1o);
+		initial0s = numLength(mx) - 1 - numLength(f1);
 
-		if (count != 96)
-			printf("%0.0f, ", sum);
+		while (f1o > 0 && initial0s > 0)
+		{
+			printf("%d", 0);
+			--initial0s;
+		}
+
+		printf("%lu", f1);
+
+		sum = (f1 + f2) % mx;
+		sumo = f1o + f2o + (f1 + f2) / mx;
+		f1 = f2;
+		f1o = f2o;
+		f2 = sum;
+		f2o = sumo;
+
+		if (count != 98)
+			printf(", ");
 		else
-			printf("%0.0f", sum);
-
-		fib1 = fib2;
-		fib2 = sum;
+			printf("\n");
 	}
-
-	printf("\n");
 
 	return (0);
 }
