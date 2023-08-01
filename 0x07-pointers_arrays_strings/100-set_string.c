6da7edf2fee5a9a7dc70e3bfa157c4dc22a79acd
Nahom <nahomtensae@gmail.com>
2023-07-31 19:51:33 -0700

    100, 101

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * set_string - set value of pointer to a char
+  * @s: varaiable of type pointer
+  * @to: char
+  *
+  * Return: void
+  */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
